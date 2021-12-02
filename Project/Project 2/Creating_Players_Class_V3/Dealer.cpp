59ee50a157abe471650257ff6a0be7b21983b45a
UtotingPuwet <Utotingpuwet91@yahoo.com>
2021-12-02 01:27:58 -0800

    Signed-off-by: UtotingPuwet <Utotingpuwet91@yahoo.com>

---
+++ Project/Project 2/Creating_Players_Class_V3/Dealer.cpp
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Dealer.h
+ * Author: UtotingPuwet
+ *
+ * Created on December 1, 2021, 6:26 PM
+ */
+
+#include "Dealer.h"
+#include <iostream>
+using namespace std;
+
+
+Dealer::Dealer() {
+    int random = rand()%5;
+    name += "Dealer " + names[random];
+    hand = 0;
+}
+
+int Dealer::iniHand() {
+    int random = rand()%52;
+    hand += dealDek.deal(random,name);
+    random = rand()%52;
+    hand += dealDek.hidDeal(random);
+    return hand;
+}
+
+int Dealer::draw(int random) {
+    int n = dealDek.deal(random,name);
+    if (n == 1 && hand < 10) {
+        cout << name << " got a ";
+        return hand += 11;
+    }
+    else {
+        cout << name << " got a ";
+        return hand += n;
+    }
+    return hand += n;
+}
+
+
+string Dealer::names[5] = {"Aisha", "Annallea", "Adrianna", "Ashley", "Amber"};
