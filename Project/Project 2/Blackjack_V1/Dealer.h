8d110ec0c9eabdb2fe1e8c4780c3a7c0f3309312
UtotingPuwet <Utotingpuwet91@yahoo.com>
2021-12-02 17:01:30 -0800

    Signed-off-by: UtotingPuwet <Utotingpuwet91@yahoo.com>

---
+++ Project/Project 2/Blackjack_V1/Dealer.h
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Dealer.h
+ * Author: UtotingPuwet
+ *
+ * Created on December 1, 2021, 6:26 PM
+ */
+
+#ifndef DEALER_H
+#define DEALER_H
+#include "Player.h"
+
+class Dealer : public AbsPlay{
+private:
+    int hand;
+    string name;
+    DealDek dealDek;
+    static string names[5];
+public:
+    Dealer();
+    
+    int getHand() {return hand;}
+    string getName() {return name;} 
+    
+    int iniHand();
+    int draw(int);
+};
+
+
+#endif /* DEALER_H */
+
